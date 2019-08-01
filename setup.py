f73f17266f887a9b2cf0e575bc6824bb260bb0a3
jessicametzger <40182204+jessicametzger@users.noreply.github.com>
2019-08-01 19:47:11 +0900

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import find_packages, setup
+
+setup(name = 'grcrfit',
+    packages = find_packages(),
+    description = 'Fit local cosmic ray + gamma ray data',
+    author = 'Jessica Metzger',
+    author_email = 'jmetzger@uchicago.edu',
+    url = 'https://github.com/jessicametzger/grcrfit')
