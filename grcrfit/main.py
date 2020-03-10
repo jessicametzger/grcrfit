a6c5a3b0b7a752571a3ad4c9f8ebe831fe84f2c9
Tsunefumi Mizuno <mizuno@astro.hiroshima-u.ac.jp>
2020-03-10 14:41:34 +0900

    add comments to grcrfit/main.py.

--- grcrfit/main.py
+++ grcrfit/main.py
@@ -12,8 +12,8 @@ from .run import Run
 # savesteps is how many steps to save in walkers.dat (if larger than nsteps, will be nsteps)
 def run_fit(flag, fdict, rerun=False, nsteps=5000, nwalkers=None, PT=True, ntemps=10, processes=None,
             modflags={'pl': 's', 'enh': 0, 'weights': None, 'priors': 0, 'crscaling': False,
-                      'grscaling': False, 'fixd': None, 'enhext': False, 'priorlimits': False,
-                      'one_d': True},
+                      'grscaling': False, 'enhext': False, 'priorlimits': False, 'vphi_err': 100.,
+                      'fixd': None, 'one_d': True, 'fix_vphi': None},
             save_steps=1000.):
     
     # initialize run
