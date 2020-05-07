29e79ca40d1c5624bd9acb6ebf6e649f01d24e16
Tsunefumi Mizuno <mizuno@astro.hiroshima-u.ac.jp>
2020-05-07 13:55:25 +0900

    modify grcrfit/grfluxes.py to implement a hybrid model for pp-interaction (Kamae +06 (non-diffractive) below 10 GeV and and AAfrag above that).

--- grcrfit/grfluxes.py
+++ grcrfit/grfluxes.py
@@ -18,11 +18,17 @@ from . import physics as ph
 path = os.getcwd()+'/'
 
 # open gamma-ray cross-section (ds/dE) contribution table
-gammads = h.lstoarr(h.open_stdf(path+'data/gammads.dat'),',').astype(np.float)
+gammads = h.lstoarr(h.open_stdf(path+'data/gammads.dat'),',').astype(np.float) # Kamae +06
 Tps = gammads[1:,0] # incoming proton energy
 Egs = gammads[0,1:] # outgoing gamma energy
 logEgs = np.log10(Egs)
 ds = gammads[1:,1:]*Egs/(4*np.pi) #convert to right emissivity units
+
+gammads_hybrid = h.lstoarr(h.open_stdf(path+'data/gammads_hybrid.dat'),',').astype(np.float) # hybrid (Kamae +06 (non-diffractive) below 10 GeV and AAfrag above that)
+Tps_hybrid = gammads_hybrid[1:,0] # incoming proton energy
+Egs_hybrid = gammads_hybrid[0,1:] # outgoing gamma energy
+logEgs_hybrid = np.log10(Egs_hybrid)
+ds_hybrid = gammads_hybrid[1:,1:]*Egs_hybrid/(4*np.pi) #convert to right emissivity units
 
 widths=np.empty(Tps.shape)
 for j in range(Tps.shape[0]):
@@ -34,11 +40,23 @@ for j in range(Tps.shape[0]):
     elif (j > 38):
         widths[j] = .5*widths[j]
 
+widths_hybrid=np.empty(Tps_hybrid.shape)
+for j in range(Tps_hybrid.shape[0]):
+    widths_hybrid[j] = Tps_hybrid[j]*.348; #MeV
+    if (j < 38):
+        widths_hybrid[j] = 1*widths_hybrid[j]
+    elif (j == 38):
+        widths_hybrid[j] = .75*widths_hybrid[j]
+    elif (j > 38):
+        widths_hybrid[j] = .5*widths_hybrid[j]
+
 # proton momenta at Tps
 Pps = ph.E_to_p(Tps, ph.M_DICT['h'])
+Pps_hybrid = ph.E_to_p(Tps_hybrid, ph.M_DICT['h'])
 
 # interpolater assumes ~linear relationship between ds/dE and log Eg
 interps = [interpolate.interp1d(logEgs, ds[i,:], kind='linear', fill_value='extrapolate') for i in range(ds.shape[0])]
+interps_hybrid = [interpolate.interp1d(logEgs_hybrid, ds_hybrid[i,:], kind='linear', fill_value='extrapolate') for i in range(ds_hybrid.shape[0])]
 
 # only from p-p interactions; will be enhanced
 def get_fluxes_pp(LIS_params_pp, GRlogEgs, crfunc):
@@ -58,3 +76,22 @@ def get_fluxes_pp(LIS_params_pp, GRlogEgs, crfunc):
             except: return -np.inf
     
     return GRfluxes
+
+def get_fluxes_pp_hybrid(LIS_params_pp, GRlogEgs, crfunc):
+    
+    #get proton CR flux at Tps, cancel out sr^-1, convert area^-1 units to mbarn^-1
+    Jps_hybrid = crfunc(LIS_params_pp, Pps_hybrid, 'h')*4*np.pi*1e-34
+    
+    GRfluxes_hybrid = []
+    for i in range(len(GRlogEgs)):
+        GRfluxes_hybrid += [np.zeros(GRlogEgs[i].shape)]
+        
+        for j in range(Tps_hybrid.shape[0]):
+            
+            # interpolate at desired GR energies for the current Tp
+            # weight by proton flux at current Tp, dTp (bin width)
+            try: GRfluxes_hybrid[i] += interps_hybrid[j](GRlogEgs[i])*Jps_hybrid[j]*widths_hybrid[j]
+            except: return -np.inf
+    
+    return GRfluxes_hybrid
+
